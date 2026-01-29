cd8c77b6cdd4c3239746e67a506559eb27205779
Nader PJR <pjrnader@gmail.com>
2026-01-29 22:02:46 +0430

    a little bit change to header section

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <div className="relative w-full h-screen  text-black rounded-lg flex flex-col justify-center items-center overflow-hidden">
+        <div className="relative w-full h-screen text-black rounded-lg flex flex-col justify-center items-center overflow-hidden">
 
-            <div className="w-80 h-80  absolute -top-18 -left-18 blur-3xl overflow-hidden"></div>
-            <div className="w-80 h-80  absolute bottom-38 -right-18 blur-3xl overflow-hidden"></div>
+            <div className="w-80 h-80 bg-accent absolute top-48 -left-38 blur-3xl overflow-hidden"></div>
+            <div className="w-80 h-80 bg-accent absolute bottom-39 -right-38 blur-3xl overflow-hidden"></div>
 
             <div className="w-full md:max-w-2xl absolute px-10 md:px-0 text-center flex flex-col justify-center items-center">
                 <h1 className="text-blue text-3xl mb-3 font-extrabold leading-15  md:text-5xl">پلتفرم آموزشی و توانمندی سازی دختران در افغانستان</h1>
@@ -14,7 +14,7 @@ export default function Header() {
                     {/* <Link href="/signup" className="bg-blue-500/65 backdrop-blur-3xl text-white px-4 py-2 rounded mx-2 hover:bg-blue-500 transition">
                         ثبت‌نام
                     </Link> */}
-                    <Link href="/login" className="bg-accent  text-white backdrop-blur-3xl mx-2 px-7 py-3 rounded-2xl transition">
+                    <Link href="/login" className="bg-accent hover:bg-blue-800/80  text-white backdrop-blur-3xl mx-2 px-7 py-3 rounded-2xl transition">
                         شروع یادگیری
                     </Link>
                 </div>
